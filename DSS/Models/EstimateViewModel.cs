d546d6d555721faf238e89136b576beb2e445e40
nidroy <nidroy@mail.ru>
2024-01-05 17:10:06 +0300

    Добавлена модель сметы.
    Добавлен .gitignore для Visual Studio.

---
+++ DSS/Models/EstimateViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DSS.Models
+{
+    public class EstimateViewModel
+    {
+        public string? Name { get; set; }
+        public double LevelOfWork { get; set; }
+        public double Cost { get; set; }
+        public string? Link { get; set; }
+        public int RoadId { get; set; }
+    }
+}
