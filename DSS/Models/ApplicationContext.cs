0fca9da384ad6dc5c2f7003496489918dd120c61
nidroy <nidroy@mail.ru>
2024-02-14 17:56:52 +0300

    Обновлены данные смет.
    Изменены названия смет.

--- DSS/Models/ApplicationContext.cs
+++ DSS/Models/ApplicationContext.cs
@@ -41,13 +41,37 @@ namespace DSS.Models
                 ("18 ОП РЗ 18Р-2-10", 1)
             };
 
+            List<string> estimatesData = new()
+            {
+                "планирование и проведение регулярной диагностики состояния дорожного покрытия",
+                "поддержание и ремонт асфальтового покрытия",
+                "ремонт трещин и ям на дорожном покрытии",
+                "укладку нового асфальтового покрытия",
+                "очистку и обновление дорожных обочин",
+                "маркировку и разметку дорог для обеспечения безопасности движения",
+                "установку и обновление дорожных знаков и сигнальной арматуры",
+                "обслуживание и ремонт дорожных ограждений и барьеров",
+                "поддержание и обновление дорожного освещения",
+                "ремонт и обслуживание дренажной системы для отвода воды с дорожной поверхности",
+                "очистку и обслуживание дренажных канав",
+                "поддержание и ремонт мостов, путепроводов и путеподъездных путей",
+                "укрепление и ремонт обочин и береговых склонов",
+                "очистку и обслуживание дорожных тоннелей и подземных пешеходных переходов",
+                "поддержание и ремонт дорожных сооружений, таких как шумоизоляционные экраны",
+                "установку и обновление средств пожаротушения и аварийного оборудования",
+                "ремонт и обслуживание дорожных септиков и систем водоотведения",
+                "поддержание и ремонт дорожных канализационных систем",
+                "очистку и обновление зеленых насаждений вдоль дорог",
+                "разработку и реализацию мероприятий по снижению загрязнения окружающей среды от дорожного движения"
+            };
+
             int estimateCount = 0;
 
             for (int i = 0; i < 20; i++)
             {
                 (string roadNumber, double roadPriority) = roadsData[i];
                 Road road = CreateRoadModel(i + 1, roadNumber, roadPriority, modelBuilder);
-                estimateCount = CreateEstimateModels(estimateCount, road, modelBuilder);
+                estimateCount = CreateEstimateModels(estimateCount, estimatesData, road, modelBuilder);
             }
 
             CreateTechnicalConditionOfRoadModels(modelBuilder);
@@ -67,14 +91,14 @@ namespace DSS.Models
             return road;
         }
 
-        private int CreateEstimateModels(int estimateCount, Road road, ModelBuilder modelBuilder)
+        private int CreateEstimateModels(int estimateCount, List<string> estimatesData, Road road, ModelBuilder modelBuilder)
         {
             for (int i = 1; i <= 20; i++)
             {
                 Estimate estimate = new()
                 {
                     Id = i + estimateCount,
-                    Name = $"Смета {i + estimateCount}",
+                    Name = $"Смета на {estimatesData[i - 1]}",
                     LevelOfWorks = Math.Round(i * 0.2, 2),
                     Cost = Math.Round(i * 0.2 * 100000, 2),
                     Link = "",
